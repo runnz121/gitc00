1bf20dc0685f819b3d709aeb7601e5dccc64d22f
jongbpar <jongbpar@c8r5s4.42seoul.kr>
2021-03-27 23:44:34 +0900

    t

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jongbpar <jongbpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/27 17:25:34 by jongbpar          #+#    #+#             */
+/*   Updated: 2021/03/27 17:32:11 by jongbpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+	int	i;
+
+	letter = 'z';
+	i = 1;
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}
