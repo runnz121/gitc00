862d567d72ef24b6475f6f2df4880fe704344fc8
Hey E Sang Hae See <54499829+runnz121@users.noreply.github.com>
2021-04-01 14:48:25 +0900

    Add files via upload

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: jongbpar <jongbpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 01:56:21 by jongbpar          #+#    #+#             */
-/*   Updated: 2021/03/29 13:19:25 by jongbpar         ###   ########.fr       */
+/*   Updated: 2021/04/01 14:46:55 by jongbpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,34 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_print(int a, int b)
+{
+	ft_putchar(a / 10 + '0');
+	ft_putchar(a % 10 + '0');
+	ft_putchar(' ');
+	ft_putchar(b / 10 + '0');
+	ft_putchar(b % 10 + '0');
+	if (a != 98)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
 void	ft_print_comb2(void)
 {
 	int	a;
 	int	b;
 
-	a = -1;
-	while (a++ < 98)
+	a = 0;
+	while (a <= 98)
 	{
-		b = a;
-		while (b++ < 99)
+		b = a + 1;
+		while (b <= 99)
 		{
-			ft_putchar('0' + (a / 10));
-			ft_putchar('0' + (a % 10));
-			ft_putchar(' ');
-			ft_putchar('0' + (b / 10));
-			ft_putchar('0' + (b % 10));
-			if (a != 98)
-			{
-				ft_putchar(',');
-				ft_putchar(' ');
-			}
+			ft_print(a, b);
+			b++;
 		}
+		a++;
 	}
 }
